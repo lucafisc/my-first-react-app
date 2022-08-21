cc2919737508adaede7dd5fffb1251aa76d7e72d
lucafisc <lucafischer@fastmail.com>
2022-08-21 19:35:04 +0200

    Add Footer

--- src/modules/Footer.js
+++ src/modules/Footer.js
@@ -1,8 +1,16 @@
+import GithubIcon from "../images/GitHub.png";
+import FacebookIcon from "../images/Facebook.png";
+import TwitterIcon from "../images/Twitter.png";
+import InstagramIcon from "../images/Instagram.png";
+
 function Footer() {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <footer className="footer">
+      <img src={TwitterIcon} />
+      <img src={FacebookIcon} />
+      <img src={GithubIcon} />
+      <img src={InstagramIcon} />
+    </footer>
   );
 }
 
