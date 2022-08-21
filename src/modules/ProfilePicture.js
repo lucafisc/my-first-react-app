ee5b975f09ba0c990b3c2b62361c5c9c2d3b3e2c
lucafisc <lucafischer@fastmail.com>
2022-08-21 19:46:06 +0200

    Add image credits

--- src/modules/ProfilePicture.js
+++ src/modules/ProfilePicture.js
@@ -2,9 +2,21 @@ import dogSrc from "../images/dog.jpg";
 
 function ProfilePicture() {
   return (
-    <div className="avatar">
-      <img src={dogSrc} />
-    </div>
+    <>
+      <div className="avatar">
+        <div className="credits">
+          Photo by{" "}
+          <a href="https://unsplash.com/@alecuffia?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            José Alejandro Cuffia
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com/s/photos/profile?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </div>
+        <img src={dogSrc} />
+      </div>
+    </>
   );
 }
 
